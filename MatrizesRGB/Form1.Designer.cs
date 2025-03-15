c1608ae2808c55678d9c309a6f9af9593c85d727
CarlosBueno98101 <103401@aluno.uricer.edu.br>
2025-03-15 00:00:20 -0300

    Função Brilho/Unir

--- MatrizesRGB/Form1.Designer.cs
+++ MatrizesRGB/Form1.Designer.cs
@@ -28,13 +28,266 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBoxC = new System.Windows.Forms.PictureBox();
+            this.imgBButton = new System.Windows.Forms.Button();
+            this.imgAButton = new System.Windows.Forms.Button();
+            this.pictureBoxB = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBoxA = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.valorEntrada = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonSub = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxA)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valorEntrada)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(777, 716);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.pictureBoxC);
+            this.tabPage1.Controls.Add(this.imgBButton);
+            this.tabPage1.Controls.Add(this.imgAButton);
+            this.tabPage1.Controls.Add(this.pictureBoxB);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.pictureBoxA);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(769, 687);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Juntar";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(40, 499);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 44);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Subtrair Imagens";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(540, 499);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(151, 44);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Somar Imagens";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBoxC
+            // 
+            this.pictureBoxC.Location = new System.Drawing.Point(233, 389);
+            this.pictureBoxC.Name = "pictureBoxC";
+            this.pictureBoxC.Size = new System.Drawing.Size(269, 277);
+            this.pictureBoxC.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxC.TabIndex = 11;
+            this.pictureBoxC.TabStop = false;
+            // 
+            // imgBButton
+            // 
+            this.imgBButton.Location = new System.Drawing.Point(500, 315);
+            this.imgBButton.Name = "imgBButton";
+            this.imgBButton.Size = new System.Drawing.Size(151, 44);
+            this.imgBButton.TabIndex = 10;
+            this.imgBButton.Text = "Adicionar Imagem";
+            this.imgBButton.UseVisualStyleBackColor = true;
+            this.imgBButton.Click += new System.EventHandler(this.imgBButton_Click);
+            // 
+            // imgAButton
+            // 
+            this.imgAButton.Location = new System.Drawing.Point(74, 315);
+            this.imgAButton.Name = "imgAButton";
+            this.imgAButton.Size = new System.Drawing.Size(151, 44);
+            this.imgAButton.TabIndex = 9;
+            this.imgAButton.Text = "Adicionar Imagem";
+            this.imgAButton.UseVisualStyleBackColor = true;
+            this.imgAButton.Click += new System.EventHandler(this.imgAButton_Click);
+            // 
+            // pictureBoxB
+            // 
+            this.pictureBoxB.Location = new System.Drawing.Point(434, 32);
+            this.pictureBoxB.Name = "pictureBoxB";
+            this.pictureBoxB.Size = new System.Drawing.Size(269, 277);
+            this.pictureBoxB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxB.TabIndex = 8;
+            this.pictureBoxB.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(537, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Imagem B";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Imagem A";
+            // 
+            // pictureBoxA
+            // 
+            this.pictureBoxA.Location = new System.Drawing.Point(25, 32);
+            this.pictureBoxA.Name = "pictureBoxA";
+            this.pictureBoxA.Size = new System.Drawing.Size(269, 277);
+            this.pictureBoxA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxA.TabIndex = 5;
+            this.pictureBoxA.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.pictureBox2);
+            this.tabPage2.Controls.Add(this.valorEntrada);
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Controls.Add(this.buttonAdd);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.buttonSub);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(769, 687);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Brilho";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(478, 33);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(269, 277);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // valorEntrada
+            // 
+            this.valorEntrada.Location = new System.Drawing.Point(317, 105);
+            this.valorEntrada.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.valorEntrada.Name = "valorEntrada";
+            this.valorEntrada.Size = new System.Drawing.Size(104, 22);
+            this.valorEntrada.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(20, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(269, 277);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(317, 236);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(104, 25);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Adicionar";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(73, 345);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Adicionar Imagem";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonSub
+            // 
+            this.buttonSub.Location = new System.Drawing.Point(317, 190);
+            this.buttonSub.Name = "buttonSub";
+            this.buttonSub.Size = new System.Drawing.Size(104, 25);
+            this.buttonSub.TabIndex = 3;
+            this.buttonSub.Text = "Subtrair";
+            this.buttonSub.UseVisualStyleBackColor = true;
+            this.buttonSub.Click += new System.EventHandler(this.buttonSub_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(775, 713);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxA)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valorEntrada)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.NumericUpDown valorEntrada;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonSub;
+        private System.Windows.Forms.Button imgBButton;
+        private System.Windows.Forms.Button imgAButton;
+        private System.Windows.Forms.PictureBox pictureBoxB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBoxA;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBoxC;
     }
 }
 
--- MatrizesRGB/Form1.cs
+++ MatrizesRGB/Form1.cs
@@ -10,11 +10,246 @@ using System.Windows.Forms;
 
 namespace MatrizesRGB
 {
+
     public partial class Form1: Form
     {
+        Bitmap img1;
+        Bitmap img2;
+        byte[,] vImg1Gray;
+
+        byte[,] vImg1R;
+        byte[,] vImg1G;
+        byte[,] vImg1B;
+        byte[,] vImg1A;
+        bool bLoadImgOK = false;
         public Form1()
         {
             InitializeComponent();
+        }
+       
+        private void carregarImg(PictureBox pictureBox)
+        {
+            // Configurações iniciais da OpenFileDialogBox
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            var filePath = string.Empty;
+            openFileDialog1.InitialDirectory = "C:\\Users\\cadue\\Documents\\MatLab\\Material Matlab\\Matlab";
+            openFileDialog1.Filter = "TIFF image (*.tif)|*.tif|JPG image (*.jpg)|*.jpg|BMP image (*.bmp)|*.bmp|PNG image (*.png)|*.png|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            // Se um arquivo foi localizado com sucesso...
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Armnazena o path do arquivo de imagem
+                filePath = openFileDialog1.FileName;
+
+                try
+                {
+                    img1 = new Bitmap(filePath);
+                    img2 = new Bitmap(img1.Width, img1.Height);
+                    bLoadImgOK = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Erro ao abrir imagem...", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    bLoadImgOK = false;
+                }
+
+                // Se a imagem carregou perfeitamente...
+                if (bLoadImgOK == true)
+                {
+                    // Adiciona imagem na PictureBox
+                    pictureBox.Image = img1;
+                    vImg1Gray = new byte[img1.Width, img1.Height];
+                    vImg1R = new byte[img1.Width, img1.Height];
+                    vImg1G = new byte[img1.Width, img1.Height];
+                    vImg1B = new byte[img1.Width, img1.Height];
+                    vImg1A = new byte[img1.Width, img1.Height];
+                }
+
+                // Percorre todos os pixels da imagem...
+
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < img1.Width; i++)
+            {
+                for (int j = 0; j < img1.Height; j++)
+                {
+                    Color pixel = img1.GetPixel(i, j);
+
+                    int finalR = pixel.R + (int)valorEntrada.Value;
+                    int finalG = pixel.G + (int)valorEntrada.Value;
+                    int finalB = pixel.B + (int)valorEntrada.Value;
+
+                    if (finalR > 255)
+                    {
+                        finalR = 255;
+                    }
+
+                    if (finalG > 255)
+                    {
+                        finalG = 255;
+                    }
+
+                    if (finalB > 255)
+                    {
+                        finalB = 255;
+                    }
+
+
+                    Color cor = Color.FromArgb(255, finalR, finalG, finalB);
+
+                    img2.SetPixel(i, j, cor);
+                }
+            }
+            pictureBox2.Image = img2;
+        }
+
+        private void buttonSub_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < img1.Width; i++)
+            {
+                for (int j = 0; j < img1.Height; j++)
+                {
+                    Color pixel = img1.GetPixel(i, j);
+
+                    int finalR = pixel.R - (int)valorEntrada.Value;
+                    int finalG = pixel.G - (int)valorEntrada.Value;
+                    int finalB = pixel.B - (int)valorEntrada.Value;
+
+                    if (finalR < 0)
+                    {
+                        finalR = 0;
+                    }
+
+                    if (finalG < 0)
+                    {
+                        finalG = 0;
+                    }
+
+                    if (finalB < 0)
+                    {
+                        finalB = 0;
+                    }
+
+
+                    Color cor = Color.FromArgb(255, finalR, finalG, finalB);
+
+                    img2.SetPixel(i, j, cor);
+                }
+            }
+            pictureBox2.Image = img2;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            carregarImg(pictureBox1);
+        }
+        private void imgAButton_Click(object sender, EventArgs e)
+        {
+            carregarImg(pictureBoxA);
+        }
+        private void imgBButton_Click(object sender, EventArgs e)
+        {
+            carregarImg(pictureBoxB);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap imgA = new Bitmap(pictureBoxA.Image);
+            Bitmap imgB = new Bitmap(pictureBoxB.Image);
+
+            if (imgA.Width != imgB.Width || imgA.Height != imgB.Height)
+            {
+                MessageBox.Show("As imagens devem ter o mesmo tamanho", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            } else
+            {
+                Bitmap imgC = new Bitmap(imgA.Width, imgA.Height);
+                for (int i = 0; i < imgA.Width; i++)
+                {
+                    for (int j = 0; j < imgA.Height; j++)
+                    {
+                        Color pixelA = imgA.GetPixel(i, j);
+                        Color pixelB = imgB.GetPixel(i, j);
+
+                        int finalR = pixelA.R + pixelB.R;
+                        int finalG = pixelA.G + pixelB.G;
+                        int finalB = pixelA.B + pixelB.B;
+
+                        if (finalR > 255)
+                        {
+                            finalR = 255;
+                        }
+
+                        if (finalG > 255)
+                        {
+                            finalG = 255;
+                        }
+
+                        if (finalB > 255)
+                        {
+                            finalB = 255;
+                        }
+
+                        Color cor = Color.FromArgb(255, finalR, finalG, finalB);
+
+                        imgC.SetPixel(i, j, cor);
+                    }
+                }
+                pictureBoxC.Image = imgC;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap imgA = new Bitmap(pictureBoxA.Image);
+            Bitmap imgB = new Bitmap(pictureBoxB.Image);
+
+            if (imgA.Width != imgB.Width || imgA.Height != imgB.Height)
+            {
+                MessageBox.Show("As imagens devem ter o mesmo tamanho", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            else
+            {
+                Bitmap imgC = new Bitmap(imgA.Width, imgA.Height);
+                for (int i = 0; i < imgA.Width; i++)
+                {
+                    for (int j = 0; j < imgA.Height; j++)
+                    {
+                        Color pixelA = imgA.GetPixel(i, j);
+                        Color pixelB = imgB.GetPixel(i, j);
+
+                        int finalR = pixelA.R - pixelB.R;
+                        int finalG = pixelA.G - pixelB.G;
+                        int finalB = pixelA.B - pixelB.B;
+
+                        if (finalR < 0)
+                        {
+                            finalR = 0;
+                        }
+
+                        if (finalG < 0)
+                        {
+                            finalG = 0;
+                        }
+
+                        if (finalB < 0)
+                        {
+                            finalB = 0;
+                        }
+
+                        Color cor = Color.FromArgb(255, finalR, finalG, finalB);
+
+                        imgC.SetPixel(i, j, cor);
+                    }
+                }
+                pictureBoxC.Image = imgC;
+            }
         }
     }
 }
